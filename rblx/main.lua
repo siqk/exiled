ccb245ea4dea444dc6b6811280eb291181773242
cursed <ikiikikikijujijij@gmail.com>
2026-03-13 13:30:36 -0400

    Create main.lua

---
+++ rblx/main.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ccursedd/exiled/refs/heads/main/rblx/nametags.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ccursedd/exiled/refs/heads/main/rblx/emotes.lua"))()
